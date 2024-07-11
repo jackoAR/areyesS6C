eae6fecba2fa05383386e2eed2cfb7c73a3b9330
jackoAR <jhsjacko@gmail.com>
2024-07-10 21:12:24 -0500

    Botón Eliminar y Actualizar funcional

--- MauiApp1/Views/ActEliminar.xaml.cs
+++ MauiApp1/Views/ActEliminar.xaml.cs
@@ -1,5 +1,9 @@
 using MauiApp1.Models;
+using Microsoft.Maui.Controls;
+using Newtonsoft.Json;
 using System.Net;
+using System.Text;
+
 
 namespace MauiApp1.Views;
 
@@ -14,9 +18,46 @@ public partial class ActEliminar : ContentPage
         txtEdad.Text = datos.edad.ToString();
 	}
 
-    private void btnActualizar_Clicked(object sender, EventArgs e)
+    private async void btnActualizar_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            int codigo = int.Parse(txtCodigo.Text);
 
+            var datosEstudiante = new Dictionary<string, string>
+            {
+                {"codigo", codigo.ToString()},
+                {"nombre", txtNombre.Text},
+                {"apellido", txtApellido.Text},
+                {"edad",  txtEdad.Text}
+            };
+
+            
+            var query = new FormUrlEncodedContent(datosEstudiante);
+            string url = "http://192.168.200.4/moviles/post.php?" + await query.ReadAsStringAsync();
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.PutAsync(url, null);
+
+            // Verificar la respuesta
+            if (response.IsSuccessStatusCode)
+            {
+                await DisplayAlert("Éxito", "Datos actualizados correctamente", "OK");
+                await Navigation.PushAsync(new vEstudiante());
+            }
+            else
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                await DisplayAlert("Error", $"No se pudo actualizar los datos: {responseBody}", "OK");
+            }
+            
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Alerta", ex.Message, "OK");
+        }
     }
 
     private void btnEliminar_Clicked(object sender, EventArgs e)
@@ -36,7 +77,6 @@ public partial class ActEliminar : ContentPage
         }
         catch (Exception ex)
         {
-
             DisplayAlert("Alerta", ex.Message, "OK");
         }
     }
